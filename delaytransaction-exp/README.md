08c61e790565c11538b1fa67fcf781ebe0a164e6
Itleaks <itleaks@126.com>
2018-10-24 10:23:18 +0800

    Add defer transaction exp
    Signed-off-by: Itleaks <itleaks@126.com>

---
+++ delaytransaction-exp/README.md
@@ -0,0 +1,21 @@
+# Run 'delaytransaction' demo
+```
+//setup
+//mac
+npm install -g js4eos
+//ubuntu
+sudo npm install -g js4eos
+js4eos wallet create
+js4eos wallet import your_private_key
+//build
+cd delaytransaction
+js4eos compile -o delaytransaction.wasm delaytransaction.cpp 
+js4eos compile -g delaytransaction.abi delaytransaction.cpp
+//test
+cd ..
+js4eos set contract youraccount delaytransaction/
+js4eos push action youraccount hi '[1000, "testaccount"]' -p testaccount
+js4eos push action youraccount hi '[1000, "testaccount"]' -p testaccount
+js4eos push action youraccount hi '[1001, "testaccount"]' -p testaccount
+```
+如果使用cleos程序，只需将上面的js4eos替换为cleos即可
---
+++ delaytransaction-exp/delaytransaction/delaytransaction.cpp
@@ -0,0 +1,29 @@
+#include <eosiolib/eosio.hpp>
+#include <eosiolib/transaction.hpp>
+#include <eosiolib/print.hpp>
+using namespace eosio;
+
+class delaytransaction : public eosio::contract {
+  public:
+      using contract::contract;
+
+      /// @abi action
+      void hi(uint128_t id, account_name user ) {
+        print( "Hello, ", name{user} );
+        eosio::transaction txn{};
+        txn.actions.emplace_back(
+            eosio::permission_level(_self, N(active)),
+            _self,
+            N(delayedHi),
+            std::make_tuple());
+        txn.delay_sec = 360;
+        //(sender_id, payer, replace_existed)
+        txn.send(id, _self, false);
+      }
+
+      void delayedhi(uint128_t id) {
+          printf( "Delayed Hello %", id);
+      }
+};
+
+EOSIO_ABI( delaytransaction, (hi)(delayedhi) )
